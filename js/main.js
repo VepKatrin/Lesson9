afcac06b32362c40db4c2d625e1eacfaeac5d9f9
VepKatrin <KatrinKatrin@yandex.ru>
2021-12-13 01:09:32 +0300

    single page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const modal = document.querySelector('.modal');
 const btnCart = document.getElementById('cart');
 const btnClose = document.querySelector('.btn-close');
+const mainIndex = document.getElementById('index');
+const mainRestaraunt = document.getElementById('restaurant');
+const cardsBlockIndex = mainIndex.querySelector('.cards');
+const cardsIndex = cardsBlockIndex.querySelectorAll('.card');
+const logos = document.querySelectorAll('.logo-link');
+const cardsBlockRestaraunt = mainRestaraunt.querySelector('.cards');
+const cardsRestaraunt = cardsBlockRestaraunt.querySelectorAll('.card');
 const total = modal.querySelector('.modal-total');
 const rows = modal.querySelectorAll('.cart-row');
 let totalPrice = 0;
@@ -9,6 +16,11 @@ const modalOpenClose = () => {
     modal.classList.toggle('modal-close'); 
 }
 
+const toggelSection = () => {
+    mainIndex.classList.toggle('not--active');
+    mainRestaraunt.classList.toggle('not--active');
+};
+
 btnCart.addEventListener('click', modalOpenClose);
 btnClose.addEventListener('click', modalOpenClose);
 
@@ -16,6 +28,20 @@ modal.addEventListener('click', (e) => {
     if (e.target == modal) {
         modalOpenClose();
     }
+});
+
+cardsRestaraunt.forEach((card, i) => {
+    const nameProductBlock = card.querySelector('.cart-title');
+    const priceProductBlock = card.querySelector('.price');
+
+    const nameProduct = nameProductBlock.textContent;
+    const priceProduct = +priceProductBlock.textContent;
+
+    const productItem = rows[i].querySelector('.product-name');
+    const priceItem = rows[i].querySelector('.price');
+
+    productItem.textContent = nameProduct;
+    priceItem.textContent = priceProduct;
 });
 
 rows.forEach(row => {
@@ -48,3 +74,19 @@ rows.forEach(row => {
 });
 
 total.textContent = totalPrice;
+
+cardsIndex.forEach(card => {
+    card.addEventListener('click', () => {
+        toggelSection();
+    });
+});
+
+logos.forEach(logo => {
+    logo.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (mainIndex.classList.contains('not--active')) {
+            toggelSection();
+        }
+    });
+});
