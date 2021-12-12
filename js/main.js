0bd64ee16f1c1598c0b1ccca3885dc41f8cddce3
VepKatrin <KatrinKatrin@yandex.ru>
2021-12-12 18:28:59 +0300

    js calculate total and product cost

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const modal = document.querySelector('.modal');
 const btnCart = document.getElementById('cart');
 const btnClose = document.querySelector('.btn-close');
+const total = modal.querySelector('.modal-total');
+const rows = modal.querySelectorAll('.cart-row');
+let totalPrice = 0;
 
 const modalOpenClose = () => {
     modal.classList.toggle('modal-close'); 
@@ -14,3 +17,34 @@ modal.addEventListener('click', (e) => {
         modalOpenClose();
     }
 });
+
+rows.forEach(row => {
+    const priceItem = row.querySelector('.price');
+    const countItem = row.querySelector('.count');
+    const btnMunis = row.querySelector('.minus');
+    const btnPlus = row.querySelector('.plus');
+    let count = +countItem.textContent;
+    let price = +priceItem.textContent;
+
+    totalPrice += price * count;
+
+    btnMunis.addEventListener('click', () => {
+        if (count > 0) {
+            count--;
+            countItem.textContent = count;
+            priceItem.textContent = price * count;
+            totalPrice -= price;
+            total.textContent = totalPrice;
+        }
+    });
+
+    btnPlus.addEventListener('click', () => {
+        count++;
+        countItem.textContent = count;
+        priceItem.textContent = price * count;
+        totalPrice += price;
+        total.textContent = totalPrice;
+    });
+});
+
+total.textContent = totalPrice;
