1d8f171636c08086dff3afd206a2c6de7e9baa4b
VepKatrin <KatrinKatrin@yandex.ru>
2021-12-13 15:43:40 +0300

    working order form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.modal');
 const btnCart = document.getElementById('cart');
 const btnClose = document.querySelector('.btn-close');
+const btnCancel = modal.querySelector('.button--secondary');
 const mainIndex = document.getElementById('index');
 const mainRestaraunt = document.getElementById('restaurant');
 const cardsBlockIndex = mainIndex.querySelector('.cards');
@@ -8,9 +9,13 @@ const cardsIndex = cardsBlockIndex.querySelectorAll('.card');
 const logos = document.querySelectorAll('.logo-link');
 const cardsBlockRestaraunt = mainRestaraunt.querySelector('.cards');
 const cardsRestaraunt = cardsBlockRestaraunt.querySelectorAll('.card');
+const rowsBlock = modal.querySelector('.modal-main');
 const total = modal.querySelector('.modal-total');
-const rows = modal.querySelectorAll('.cart-row');
+let rows = modal.querySelectorAll('.cart-row');
 let totalPrice = 0;
+let namesArr = [];
+
+const templateRow = rows[0];
 
 const modalOpenClose = () => {
     modal.classList.toggle('modal-close'); 
@@ -21,8 +26,23 @@ const toggelSection = () => {
     mainRestaraunt.classList.toggle('not--active');
 };
 
+function getRowByName (name) {
+    let searchRow = null;
+
+    for (let row of rows) {
+        const item = row.querySelector('.product-name');
+        if (item.textContent === name) {
+            searchRow = row;
+            break;
+        }
+    }
+
+    return searchRow;
+};
+
 btnCart.addEventListener('click', modalOpenClose);
 btnClose.addEventListener('click', modalOpenClose);
+btnCancel.addEventListener('click', modalOpenClose);
 
 modal.addEventListener('click', (e) => {
     if (e.target == modal) {
@@ -33,44 +53,72 @@ modal.addEventListener('click', (e) => {
 cardsRestaraunt.forEach((card, i) => {
     const nameProductBlock = card.querySelector('.cart-title');
     const priceProductBlock = card.querySelector('.price');
+    const addToCartButtonProductBlock = card.querySelector('.button--primary');
 
     const nameProduct = nameProductBlock.textContent;
     const priceProduct = +priceProductBlock.textContent;
 
-    const productItem = rows[i].querySelector('.product-name');
-    const priceItem = rows[i].querySelector('.price');
+    let countItem = null;
+    let priceItem = null;
+    let productItem = null;
+    let count = 0;
 
-    productItem.textContent = nameProduct;
-    priceItem.textContent = priceProduct;
-});
+    addToCartButtonProductBlock.addEventListener('click', () => {
 
-rows.forEach(row => {
-    const priceItem = row.querySelector('.price');
-    const countItem = row.querySelector('.count');
-    const btnMunis = row.querySelector('.minus');
-    const btnPlus = row.querySelector('.plus');
-    let count = +countItem.textContent;
-    let price = +priceItem.textContent;
+        if (namesArr.includes(nameProduct)) {
+            const existRow = getRowByName(nameProduct);
+            countItem = existRow.querySelector('.count');
+            priceItem = existRow.querySelector('.price');
 
-    totalPrice += price * count;
-
-    btnMunis.addEventListener('click', () => {
-        if (count > 0) {
-            count--;
+            count = +countItem.textContent;
+            count++;
             countItem.textContent = count;
-            priceItem.textContent = price * count;
-            totalPrice -= price;
-            total.textContent = totalPrice;
+            priceItem.textContent = priceProduct * count;
         }
-    });
+        else {
+            namesArr.push(nameProduct);
+            const newRow = templateRow.cloneNode(true);
+            newRow.classList.remove('not--active');
 
-    btnPlus.addEventListener('click', () => {
-        count++;
-        countItem.textContent = count;
-        priceItem.textContent = price * count;
-        totalPrice += price;
+            productItem = newRow.querySelector('.product-name');
+            priceItem = newRow.querySelector('.price');
+            countItem = newRow.querySelector('.count');
+            const btnMunis = newRow.querySelector('.minus');
+            const btnPlus = newRow.querySelector('.plus');
+
+            count = +countItem.textContent;
+            productItem.textContent = nameProduct;
+            priceItem.textContent = priceProduct;
+
+            btnMunis.addEventListener('click', (e) => {
+                if (count > 0) {
+                    count--;
+                    countItem.textContent = count;
+                    priceItem.textContent = priceProduct * count;
+                
+                    totalPrice -= priceProduct;
+                    total.textContent = totalPrice;
+                }
+            });
+        
+            btnPlus.addEventListener('click', (e) => {
+                count++;
+                countItem.textContent = count;
+                priceItem.textContent = priceProduct * count;
+
+                totalPrice += priceProduct;
+                total.textContent = totalPrice;
+            });
+
+            rowsBlock.append(newRow);
+
+            rows = modal.querySelectorAll('.cart-row');
+        }
+
+        totalPrice += priceProduct;
         total.textContent = totalPrice;
     });
+
 });
 
 total.textContent = totalPrice;
